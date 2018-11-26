43ccc4491057ce012a96346f3d6d96567a2332cb
msjlwhite <msjlwhite@gmail.com>
2018-11-26 16:41:45 -0600

    JS Practice 2

---
+++ htmlPageReview.js
@@ -0,0 +1,19 @@
+function dateAndTime() 
+{
+    document.getElementById('dateAndTime').innerHTML = Date()
+}
+
+function conosolePrint()
+{
+    console.log("hello Jon");
+}
+
+function alertPrint()
+{
+    alert("hello Jon");
+}
+
+function documentPrint()
+{
+    document.write("hello jon")
+}
