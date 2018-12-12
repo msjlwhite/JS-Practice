e2baad98a8c928676a223fe77425aae33171c1b0
msjlwhite <msjlwhite@gmail.com>
2018-12-12 10:37:21 -0600

    Chapter 5 Exercise A 12.12.2018

---
+++ flattening.js
@@ -0,0 +1,24 @@
+// J White
+// Chapter 5 - Exercises
+// Goal: Use the reduce method in combination with the concat method to “flatten” 
+// an array of arrays into a single array that has all the elements of the original arrays.
+
+/* let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+// → [1, 2, 3, 4, 5, 6] */
+
+// Variables
+var arrays = [[1, 2, 3], [4, 5], [6]];
+
+// function
+function flatArray (arrays)
+{
+   //  for (var i = 0; i < arrays.length; i++)
+   // {
+        Console.log(reduce(arrays));
+  //  }
+ 
+} //EOF
+
+// function call
+flatArray();
